3fd43f15e08b01355ef7a21f4270d01d9aa6d90c
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-08-20 17:34:54 +0200

    Initial Commit

---
+++ Meme_projet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meme_projet
+//
+//  Created by Kemal Kazanc on 20/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meme_projet/Meme_projetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_projetApp.swift
+//  Meme_projet
+//
+//  Created by Kemal Kazanc on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meme_projetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
